223ea38c24bf6c605e4ee31f488f1caadbf5dc2f
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-05 23:53:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,28 @@ import React from 'react'
 import '../styles/App.css';
 import{useState} from 'react
 const App = () => {
-  const [data, setData] = useState("")
-  
+  const [data1, setData1] = useState("")
+   const [data2, setData2] = useState("")
     const handleInput1 = (event) =>{
         const input1 = event.target.value
-          setData(input1)
+          setData1(input1)
           console.log(input1)
         }
         const handleInput2 = (event) =>{
           const input2 = event.target.value
-            setData(input2)
+            setData2(input2)
           console.log(input2)
         }
   // do not change id of input elements
   return (
     <div id="main">
-       <h1>{data}</h1>
+       <h1>{data1}</h1>
       <label htmlFor='text-input'>Text Input:- </label>
       <input id="text-input" type={'text'} onChange = {handleInput1}/>
 
       <br/>
       <br/>
-
+        <h1>{data2}</h1>
       <label htmlFor='num-input'>Number input</label>
       <input id="num-input"  type={'number'} onChange = {handleInput2} />
       <br/>
